a7f46c051cd73c602f717969c3f6c94721e700a3
Craig Banach <craigsb93@gmail.com>
2026-04-16 23:00:21 +0100

    Replace old init with clean entrypoint

--- init.lua
+++ init.lua
@@ -1,25 +1,4 @@
-print("Welcome, Craig.")
-
-require("craigbanach")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>a", function() print "hi" end)
-
-require("lazy").setup({ { import = "craigbanach.plugins" } }, {
-    install = {
-        colorscheme = { "habamax" },
-    },
-})
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+require("config.lazy")
